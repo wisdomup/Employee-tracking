d128ccea862e2a765297fb9a049001f96b34230a
Asad Ali <asad@adly.com>
2026-03-17 22:08:11 +0500

    testing cicd

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -58,7 +58,7 @@ app.use('/api/upload', uploadRouter);
 app.use('/api/catalogs', catalogsRouter);
 
 app.get('/', (req: Request, res: Response) => {
-  res.status(200).json({ message: 'GPS Tracking Appppp is running' });
+  res.status(200).json({ message: 'GPS Tracking Api is running' });
 });
 
 // 404
